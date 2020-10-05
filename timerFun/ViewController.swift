64f0e000893290c8571f1c5f226f358237185ac6
Walker Gray <55608093+wakfi@users.noreply.github.com>
2020-10-05 11:29:15 -0700

    Initial Commit

---
+++ timerFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  timerFun
+//
+//  Created by Gray, John Walker on 10/5/20.
+//  Copyright © 2020 Walker Gray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+

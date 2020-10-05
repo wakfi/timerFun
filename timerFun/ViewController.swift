a81899afde99cb65b598e84c8f07975f68ea9f86
Walker Gray <55608093+wakfi@users.noreply.github.com>
2020-10-05 12:18:22 -0700

    Add timing functionality

--- timerFun/ViewController.swift
+++ timerFun/ViewController.swift
@@ -10,22 +10,52 @@ import UIKit
 
 class ViewController: UIViewController 
 {
-
   @IBOutlet var timeLabel: UILabel!
+ 
+  var seconds = 0
+  {
+    didSet
+    {
+      timeLabel.text = String(seconds)
+    }
+  }
+  
+  var timer: Timer? = nil
+  
+  func startTimer()
+  {
+    timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true)
+    { timer in
+      self.seconds += 1
+    }
+  }
+  
+  func pauseTimer()
+  {
+    guard let theTimer = timer else { return }
+    theTimer.invalidate()
+    timer = nil
+  }
+  
+  func resetTimer()
+  {
+    timeLabel.text = "0"
+    seconds = 0
+  }
   
   @IBAction func startPressed(_ sender: UIButton) 
   {
-    print("start")
+    startTimer()
   }
   
   @IBAction func pausePressed(_ sender: UIButton) 
   {
-    print("pause")
+    pauseTimer()
   }
   
   @IBAction func resetPressed(_ sender: UIButton) 
   {
-    print("reset")
+    resetTimer()
   }
   
   override func viewDidLoad() 
